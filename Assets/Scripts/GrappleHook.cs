9e6e4ea3cf6df7be0cca8ab881e1fd32c6e5c56c
Jackson Varzali <117418011+varzjac25@users.noreply.github.com>
2024-02-15 14:42:37 -0800

    Grappler appears/dissapears and can shoot up

--- Assets/Scripts/GrappleHook.cs
+++ Assets/Scripts/GrappleHook.cs
@@ -9,6 +9,7 @@ public class GrappleHook : MonoBehaviour
     [SerializeField]
     GameObject player;
     [SerializeField]
+    float grappleForce;
 
     bool grappleActive;
     
@@ -16,7 +17,8 @@ public class GrappleHook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        grappleActive = false;
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
     }
 
     // Update is called once per frame
@@ -24,11 +26,18 @@ public class GrappleHook : MonoBehaviour
     {
         if (Input.GetKeyUp(grapple))
         {
-            grappleActive = !grappleActive;
+            Debug.Log("entered");
             if (grappleActive)
             {
-                gameObject.SetActive(false);
+                gameObject.GetComponent<SpriteRenderer>().enabled = false;
             }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                gameObject.transform.position = new Vector2(player.transform.position.x, player.transform.position.y + 0.6f);
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * grappleForce, ForceMode2D.Impulse);
+            }
+            grappleActive = !grappleActive;
         }
     }
 }
