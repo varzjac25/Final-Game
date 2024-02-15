b09bda2f3943073f7fcd593b1fce828c98983101
agarish25 <agarwal.m.ishaan@gmail.com>
2024-02-15 14:51:37 -0800

    updated spear weapon with equiped weapon

--- Assets/Scripts/SpearWeapon.cs
+++ Assets/Scripts/SpearWeapon.cs
@@ -6,24 +6,34 @@ public class SpearWeapon : MonoBehaviour
 {
     [SerializeField]
     GameObject player;
+
+    [SerializeField]
+    GameObject equipedSpear;
+
+    bool hasSpear = false;
     // Start is called before the first frame update
     void Start()
     {
         gameObject.SetActive(true);
+        equipedSpear.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        while (hasSpear)
+        {
+            equipedSpear.transform.position = new Vector2(player.transform.position.x, player.transform.position.y);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            gameObject.SetActive(true);
-            transform.position = new Vector2(player.transform.position.x, player.transform.position.y - 0.5f);
+            gameObject.SetActive(false);
+            hasSpear = true;
+            
         }
     }
 }
