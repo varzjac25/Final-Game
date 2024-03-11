e47fe8584c43e681a2026a8916e0915edec09fa5
agarish25 <agarwal.m.ishaan@gmail.com>
2024-03-11 14:33:40 -0700

    made changes to color and created enemy and enemy script

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    private int health;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Spear"))
+        {
+            health -= 5;
+        }
     }
 }
